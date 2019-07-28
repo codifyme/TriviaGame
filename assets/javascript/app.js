5a7a1c2064737c8685de3c8c760a44eeb46caa3c
Angel Cochachez <acochachez@gmail.com>
2019-07-28 12:45:45 -0700

    Here Im posting an update of my files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,19 +2,19 @@ var randomQuestions = [
   {
     question:
       '1. Which soccer player has won the golden ball award the most times?',
-    answer: 'Messi'
+    answer: 'messi'
   },
   {
     question: '2. Who won the last Copa America centenario in 2019?',
-    answer: 'Chile'
+    answer: 'brazil'
   },
   {
     question: '3. Where is the next World Cup going to be held in 2022?',
-    answer: 'Russia'
+    answer: 'qatar'
   },
   {
     question: '4. Who is the best goalkeeper of 2019?',
-    answer: 'Neuer'
+    answer: 'gea'
   },
   {
     question: '5. How many FIFA World Cups has Brazil won?',
@@ -22,7 +22,7 @@ var randomQuestions = [
   },
   {
     question: '6. Who is the highest scoring Portuguese soccer player?',
-    answer: 'Ronaldo'
+    answer: 'ronaldo'
   },
   {
     question: '7. How many FIFA teams qualified for the last World Cup 2019?',
@@ -30,9 +30,12 @@ var randomQuestions = [
   },
   {
     question: '8. Which domestic league in world football is the best?',
-    answer: 'Spain-La Liga'
+    answer: 'la liga'
   }
 ];
+/* This is my randomQuestion */
+/* Incorrect answers will show up the red highlight color in every wrong question*/
+/* Note: do not use capital word because it will take as incorrect*/
 
 for (i = 0; i < randomQuestions.length; i++) {
   q = randomQuestions[i].question;
